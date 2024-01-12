953279497c6e061c0ba564a5dec148469b834337
supriyajadhav2424@gmail.com <supriyajadhav2424@gmail.com>
2024-01-12 14:01:46 +0530

    this is my second commit

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello_world():
-
-    
-  return 'Hello, Supriya! Welcome to Flask!'
-
+    return 'Hello, Supriya , Welcome to Flask app on EC2!'
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Enable debug mode for development
+    app.run(debug=False)
